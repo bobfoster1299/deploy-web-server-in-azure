0b101b6c4b4476aba7332d7e948505167b2e088e
Robert Foster <robert.foster2@nhm.ac.uk>
2020-09-09 15:26:03 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "internal" {
 
 #NSG
 resource "azurerm_network_security_group" "nsg" {
-  name                = "acceptanceTestSecurityGroup1"
+  name                = "${var.prefix}-nsg1"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
