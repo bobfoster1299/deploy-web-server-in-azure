0b6a661e01da73fe7dc27b6063dd385b2060f839
Robert Foster <robert.foster2@nhm.ac.uk>
2020-09-09 21:08:02 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -198,6 +198,6 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
   network_interface_id    = azurerm_network_interface.main.id
-  ip_configuration_name   = azurerm_network_interface.main.ip_configuration[0]
+  ip_configuration_name   = azurerm_network_interface.main.ip_configuration[0].name
   backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
 }
