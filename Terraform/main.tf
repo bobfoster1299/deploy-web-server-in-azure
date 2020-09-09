b841cfde4aaeff703ab488d12adcb71f306be156
Robert Foster <robert.foster2@nhm.ac.uk>
2020-09-09 16:10:19 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_public_ip" "public_ip" {
 
 #SCALE SET
 resource "azurerm_virtual_machine_scale_set" "main" {
-  name                = "${var.prefix}-scaleset1"
+  name                = "${var.prefix}-"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
